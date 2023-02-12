1f7342f28adcadc300d64975e963beebc58be37f
Your Name <you@example.com>
2023-02-12 18:25:29 +0300

    seminar_4_homework

---
+++ 045/Program.cs
@@ -0,0 +1,22 @@
+﻿// 45.Задать массив, заполнить случайными положительными трёхзначными числами. Показать количество нечетных\четных чисел
+
+int[] a;
+a=new int[7];
+Random rand1=new Random();
+for(int i=0;i<a.Length;i++)
+a[i]=rand1.Next(111,999);
+
+int resultEven = 0;
+int resultNotEven = 0;
+for(int i=0;i<a.Length;i++){
+    if (a[i] % 2 == 0){
+        resultEven = resultEven + 1;
+    } else {
+        resultNotEven = resultNotEven + 1;
+    }
+}
+
+System.Console.WriteLine("количество четных чисел " + $"{resultEven}"+ ", количество нечетных чисел " + $"{resultNotEven}");
+
+
+
---
+++ 046/Program.cs
@@ -0,0 +1,21 @@
+﻿// 46.Создать массив из 123 элементов. Заполнить случайными положительными числами от 0 до 1000. Найти количество элементов которые принадлежат отрезку [10,99].
+
+int [] a = new int[123];
+Random rand1 = new Random();
+for( int i=0;i<a.Length;i++)
+a[i] = rand1.Next(0,1001);
+
+int randomNumber = 0;
+for( int i=0;i<a.Length;i++){
+    if(a[i] >= 10 && a[i] <= 99){
+        randomNumber = randomNumber + 1;
+    
+    }
+    
+
+}
+
+System.Console.WriteLine (randomNumber);
+
+
+
