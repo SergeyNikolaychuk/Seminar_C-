8b8ce96acc4ce2db81c2f446a07bee37d36178e8
Your Name <you@example.com>
2023-02-15 23:00:10 +0300

    homework_5

---
+++ 051/Program.cs
@@ -0,0 +1,15 @@
+﻿// 51.С клавиатуры вводится число N. Затем вводятся N чисел. Определить сколько чисел больше 0 введено с клавиатуры
+
+int a = Convert.ToInt32(Console.ReadLine());
+int result = 0;
+    for(int i=0;i<a; i++){
+    int b =  Convert.ToInt32(Console.ReadLine());
+    if(b>0){
+        result = result + 1;
+ }
+    }
+
+System.Console.WriteLine(result);
+
+
+      
---
+++ 054/Program.cs
@@ -0,0 +1,17 @@
+﻿// 54.С клавиатуры вводится число N. Показать первые N чисел Фибоначчи. Принять первые числа равными 0 и 1
+
+int a = Convert.ToInt32(Console.ReadLine());
+
+    int first = 0;
+    int second = 1;
+    int sum = 0;
+    while(a >= sum){
+        sum = first + second;
+         Console.Write("{0} ", sum);
+        first = second;
+        second = sum;
+        
+    }
+
+  
+    
