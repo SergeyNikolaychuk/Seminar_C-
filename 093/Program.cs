e20b7a342d9be9c252e55cf8774cb1b49098d489
Your Name <you@example.com>
2023-02-28 20:22:43 +0300

    homerwork

---
+++ 093/Program.cs
@@ -0,0 +1,26 @@
+﻿//93 Задайте две матрицы. Напишите программу, которая будет находить произведение двух матриц.
+int[,] array1 = new int[,] { { 1, 2 },
+                             { 3, 4 } ,
+                              };
+
+int[,] array2 = new int[,] { { 5, 6 },
+                             { 7, 8 } };
+
+int[,] array3 = new int[array1.GetLength(0), array2.GetLength(1)];
+for (int i = 0; i < array1.GetLength(0); i++)
+{
+    for (int j = 0; j < array2.GetLength(1); j++)
+    {
+        array3[i, j] = array3[i, j] + array1[i, j] * array2[i, j];
+    }
+}
+
+for (int i = 0; i < array3.GetLength(0); i++)
+{
+    for (int j = 0; j < array3.GetLength(1); j++)
+    {
+        Console.Write(array3[i, j] + " ");
+    }
+    Console.WriteLine();
+}
+
---
+++ 098/Program.cs
@@ -0,0 +1,40 @@
+﻿// 98.Показать треугольник Паскаля
+// *Сделать вывод в виде равнобедренного треугольника
+int row = 3;
+int[,] triangele = new int[row, row];
+const int cellWindth =3;
+
+//void FillTriangle()
+{
+    for (int i = 0; i < row; i++)
+    {
+        triangele[i, 0] = 1;
+        triangele[i, 1] = 1;  
+    } 
+    for (int i = 2; i< row; i++)
+    {
+        for (int j = 1; j <= i; j ++)
+        {
+            triangele[i, j] = 
+                triangele[i - 1, j - 1 ] + triangele[i - 1, j - 1];
+        }
+    }    
+}
+void PrintTriangele()
+    {
+        for(int i = 0; i < row; i++)
+        {
+            for(int j = 0; j < row; j++)
+            {
+            if (triangele[i, j] != 0)
+                Console.Write($"{triangele[i, j],cellWindth}");
+            }
+            Console.WriteLine();
+
+        }
+        
+}    
+
+Console.ReadLine();
+PrintTriangele();
+Console.ReadLine();
